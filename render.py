b6d8cf1bdb4f32f5aa8b510b1d61fedb16d93b1a
TaniksB <krmalkama@gmail.com>
2026-05-22 09:10:27 +0200

    add setup and render

---
+++ render.py
@@ -0,0 +1,61 @@
+from pieces import Pawn, Rook, Knight, Bishop, Queen, King
+from rich import print
+
+def render(gamestate, move_white):
+    if move_white:
+        render_white(gamestate)
+    else:
+        render_black(gamestate)
+
+
+
+def render_white(gamestate):
+    board = "8 "
+    pointer_x = 1
+    pointer_y = 8
+    square_white = True
+    while pointer_y > 0:
+        # Set Piece type
+        if gamestate[pointer_y][pointer_x] is None:
+            symbol = " "
+        if isinstance(gamestate[pointer_y][pointer_x], Pawn):
+            symbol = "P"
+        if isinstance(gamestate[pointer_y][pointer_x], Rook):
+            symbol = "R"
+        if isinstance(gamestate[pointer_y][pointer_x], Knight):
+            symbol = "N"
+        if isinstance(gamestate[pointer_y][pointer_x], Bishop):
+            symbol = "B"
+        if isinstance(gamestate[pointer_y][pointer_x], Queen):
+            symbol = "Q"
+        if isinstance(gamestate[pointer_y][pointer_x], King):
+            symbol = "K"
+        # Set Piece color
+        if gamestate[pointer_y][pointer_x] != None and gamestate[pointer_y][pointer_x].white:
+            symbol = f"[bold white]{symbol}[/bold white]"
+        else:
+            symbol = f"[bold black]{symbol}[/bold black]"
+        # Set Square color and build full string
+        if square_white:
+            square = f"[white][[/white]{symbol}[white]][/white]"
+        else:
+            square = f"[black][[/black]{symbol}[black]][/black]"
+        board += square
+        if pointer_x == 8:
+            # add row number here later
+            pointer_x = 1
+            pointer_y -= 1
+            if pointer_y != 0:
+                board += f"\n{pointer_y} "
+        else:
+            pointer_x += 1
+            square_white = not square_white
+    board += "\n   a  b  c  d  e  f  g  h"
+    print(board)
+
+
+
+
+def render_black(gamestate):
+    pass
+
--- setup.py
+++ setup.py
@@ -1,12 +1,43 @@
 
+from pieces import Rook, Knight, Bishop, King, Queen, Pawn
+from render import render
 
-# Builds the starting position's gamestate
-gamestate = {}
+def setup():
+    # Builds the starting position's gamestate
+    gamestate = {}
 
-for i1 in range(1, 9):
-    gamestate[i1] = {}
-    for i2 in range(1, 9):
-        gamestate[i1][i2] = None
+    for i1 in range(1, 9):
+        gamestate[i1] = {}
+        for i2 in range(1, 9):
+            gamestate[i1][i2] = None
 
-print(gamestate)
+    gamestate[1][1] = Rook(True, 1, 1)
+    gamestate[1][2] = Knight(True, 1, 2)
+    gamestate[1][3] = Bishop(True, 1, 3)
+    gamestate[1][4] = Queen(True, 1, 4)
+    gamestate[1][5] = King(True, 1, 5)
+    gamestate[1][6] = Bishop(True, 1, 6)
+    gamestate[1][7] = Knight(True, 1, 7)
+    gamestate[1][8] = Rook(True, 1, 8)
+
+    for i in range(1, 9):
+        gamestate[2][i] = Pawn(True, 2, i)
+
+    for i in range(1, 9):
+        gamestate[7][i] = Pawn(False, 7, i)
+
+    gamestate[8][1] = Rook(False, 8, 1)
+    gamestate[8][2] = Knight(False, 8, 2)
+    gamestate[8][3] = Bishop(False, 8, 3)
+    gamestate[8][4] = Queen(False, 8, 4)
+    gamestate[8][5] = King(False, 8, 5)
+    gamestate[8][6] = Bishop(False, 8, 6)
+    gamestate[8][7] = Knight(False, 8, 7)
+    gamestate[8][8] = Rook(False, 8, 8)
+
+    return gamestate
+
+
+gst = setup()
+render(gst, True)
 
